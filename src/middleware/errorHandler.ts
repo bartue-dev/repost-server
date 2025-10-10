680036bef39dae0f2a0f29c692a18a67ca4b0727
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-10 21:35:23 +0800

    Create a errorHandler middleware

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,80 @@
+import type { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv"
+import handlePrismaError from "./prismaErrHandler.js";
+import {type PrismaErr } from "./prismaErrHandler.js";
+
+dotenv.config();
+
+type ApiError = PrismaErr & {
+  statusCode?: number,
+  message?: string,
+  isOperation?: boolean,
+  stack?: string,
+  status?: number | string,
+  name?: string
+
+}
+
+//development err
+const devErr = (res: Response, err: ApiError) => {
+  res.status(err.statusCode || 500).json({
+    status: err.statusCode,
+    message: err.message,
+    stackTrace: err.stack,
+    error: err
+  });
+}
+
+//production err
+const prodErr = (res: Response, err: ApiError) => {
+  if (err.isOperation === true) {
+    res.status(err.statusCode || 500).json({
+      status: err.statusCode,
+      message: err.message,
+      error: err
+    });
+  } else {
+    res.status(500).json({
+      status: 500,
+      message: err.message,
+      error: err
+    });
+  }
+}
+
+// prisma request err
+const prismaRequestErr = (err: PrismaErr) => {
+
+ return handlePrismaError(err)
+}
+
+// error handler middleware
+const errorHandler = (err: ApiError, req: Request, res: Response, next: NextFunction) => {
+  err.statusCode = err.statusCode || 500; //server err status code
+  err.status = err.status || "err"; // client err status code
+
+  
+  if (process.env.NODE_ENV === "development"){
+    if (err.name === "PrismaClientKnownRequestError") err = prismaRequestErr(err);
+    
+    devErr(res, err)
+  } else if (process.env.NODE_ENV === "production") {
+    if (err.name === "PrismaClientKnownRequestError") err = prismaRequestErr(err);
+
+    prodErr(res, err)
+  }
+};
+
+module.exports = errorHandler;
+
+
+/* 
+  ***two types of error***
+  OPERATIONAL ERROR: An error that can be predict
+  PROGRAMMING ERROR: An error that made by programmers
+
+
+  Handles production and development errors
+  this way is more secure and to pass a appropiate errors during development or production
+
+*/
