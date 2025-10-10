2eda18f52903454a2ba14a527975067305e360b0
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-10 21:33:13 +0800

    Remove the imported PrismaClient then import prisma from model/helper file

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "@prisma/client";
 import { fromNodeHeaders } from "better-auth/node";
 import type { Request } from "express";
+import { prisma } from "../models/helper.js";
 
-const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
       provider: "postgresql", // or "mysql", "postgresql", ...etc
