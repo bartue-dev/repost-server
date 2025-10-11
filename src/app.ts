6d58e13f558215c47806c2825ce7e57f9c27d54f
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-11 16:12:04 +0800

    Import and invoke errorHanlder middleware and postRoute with /v1/api/post endpoint

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,16 @@ import { verfiAuth } from "./middleware/authenticationMiddleware.js";
 import corsOption from "./middleware/corsOption.js"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import errorHandler from "./middleware/errorHandler.js";
+import postRoute from "./routes/postRoutes.js"
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.all("/api/auth/{*any}", toNodeHandler(auth));
+// POST http://localhost:8000/api/auth/sign-up/email
+// POST http://localhost:8000/api/auth/sign-in/email
+// Post http://localhost:8000/api/auth/sign-out
 
 //cors
 app.use(cors(corsOption));
@@ -35,11 +40,16 @@ app.use(express.json());
 app.get("/api/me", async (req: Request, res: Response) => {
     const user = await getAuthContext(req.headers);
 
-    console.log("USER:", user)
+    console.log(user)
     res.status(200).json({
-        user: user
+        user: user?.user
     })
 })
+
+app.use("/v1/api/post", postRoute)
+
+//error handler middleware
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Better Auth app listening on port ${port}`);
