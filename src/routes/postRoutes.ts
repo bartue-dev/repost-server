f03b642ec9c8dc4fd79f54a89648a932283e4fe8
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-11 16:14:48 +0800

    Creates a post route

---
+++ src/routes/postRoutes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { 
+  getAllPost, 
+  createPost, 
+  getPost, 
+  updatePost, 
+  deletePost
+} from "../controller/postController.js"
+const router = Router();
+ 
+//api route
+
+router.route("/")
+  //get all post
+  .get(getAllPost)
+  //create post
+  .post(createPost)
+
+router.route("/:id/")
+  //get specific post with postId params
+  .get(getPost)
+  //update specific post with postId params
+  .put(updatePost)
+  //delete specific post with postId params
+  .delete(deletePost)
+
+export default router
+
+
