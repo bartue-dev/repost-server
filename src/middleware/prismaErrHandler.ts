7d769c56a917daad7c1f2f3d47242a374aed6ade
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-11 16:13:44 +0800

    Import CustomErr then make meta, message and code optional type then added optional chaining to the invoke data

--- src/middleware/prismaErrHandler.ts
+++ src/middleware/prismaErrHandler.ts
@@ -1,13 +1,13 @@
-const CustomErr  = require("../utils/customErr");
+import { CustomErr } from "./customErr.js"
 
 export type PrismaErr = {
-  meta: {
+  meta?: {
     cause: string | number,
     target: string | number,
     field_name: string | number,
   }
-  message: string
-  code: string | number
+  message?: string
+  code?: string | number
 }
 
 const handlePrismaError = (err: PrismaErr) => {
@@ -15,19 +15,19 @@ const handlePrismaError = (err: PrismaErr) => {
   switch (err.code){
     case "P2001": 
       // record not found
-       return new CustomErr(`P2001: ${err.meta.cause}`, 404);
+       return new CustomErr(`P2001: ${err.meta?.cause}`, 404);
        // break;
     case "P2002": 
       // handling duplicate err
-       return new CustomErr(`P2002: ${err.meta.target} already exist`, 400);
+       return new CustomErr(`P2002: ${err.meta?.target} already exist`, 400);
        // break;
     case "P2003":
       // handling foreign key constraint
-      return new CustomErr(`P2003: Data doesn't exist in the database: ${err.meta.field_name}`, 400)
+      return new CustomErr(`P2003: Data doesn't exist in the database: ${err.meta?.field_name}`, 400)
       //break;
     case "P2025":
       // Operation failed due to records were not found
-       return new CustomErr(`P2025: ${err.meta.cause}`, 404);
+       return new CustomErr(`P2025: ${err.meta?.cause}`, 404);
        // break;
     default: 
       // handling other err
