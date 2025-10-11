c557eeabf0d11c266de8104add836c2c90927654
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-11 16:14:20 +0800

    CReates a commentQueries

---
+++ src/models/commentQueries.ts
@@ -0,0 +1,149 @@
+import { includeComment, prisma } from "./helper.js"
+import type { Comment as CommentType } from "@prisma/client";
+
+class Comment {
+  //create comment
+  async createComment(
+    comment: string,
+    userId: string,
+    postId: string
+  ) {
+    return await prisma.comment.create({
+     data: {
+      comment: comment,
+      postId: postId,
+      userId: userId
+     }
+    });
+  }
+
+  //create child comment
+  async creatChildComment(
+    comment: string,
+    userId: string, 
+    id: string
+  ) {
+    return await prisma.comment.create({
+      data: {
+        comment: comment,
+        userId: userId,
+        parentCommentId: id, 
+      }
+    })
+  }
+
+  //get all comments
+  async getAllComments(
+    postId: string, 
+    levels: number = 3
+  ) {
+    return await prisma.comment.findMany({
+      where: {
+        postId: postId,
+      },
+      include:{
+        childComment: {
+          include: includeComment(levels) 
+        } 
+      }
+    });
+  }
+
+  //get all comments with pagination
+  async getAllCommentsWithPagination(
+    postId: string, 
+    skip: number, 
+    take: number, 
+    levels: number = 3
+  ) {
+    return await prisma.comment.findMany({
+      skip: +skip,
+      take: +take,
+      where: {
+        postId: postId,
+      },
+      include:{
+        childComment: {
+          include: includeComment(levels) 
+        } 
+      }
+    });
+  }
+
+  //get child comments
+  async getChildComments(id: string) {
+    return await prisma.comment.findMany({
+      where: {
+        parentCommentId: id
+      }
+    });
+  }
+
+  //get child comments with pagination
+  async getChildCommentsWithPagination(
+    id: string, 
+    skip: number, 
+    take: number
+  ) {
+    return await prisma.comment.findMany({
+      skip: +skip,
+      take: +take,
+      where: {
+        parentCommentId: id
+      }
+    });
+  }
+
+  //update comment
+  async updateComment(
+    comment: string,
+    userId: string, 
+    id: string
+  ) {
+    return await prisma.comment.update({
+      where: {
+        id: id,
+        userId: userId
+      },
+      data: {
+        comment: comment
+      }
+    });
+  }
+
+  //delete comment recursively
+  async deleteComment(
+    id: string, 
+    userId: string
+  ) {
+    const childComment: CommentType[] = await prisma.comment.findMany({
+      where:{
+        parentCommentId: id,
+        userId: userId
+      }
+    });
+
+    //recursive delation
+    for (const child of childComment) {
+      await this.deleteComment(child.id, userId)
+    }
+
+    //during recursion delete all the children comment using parentCommentId
+    await prisma.comment.deleteMany({
+      where: {
+        parentCommentId: id,
+        userId: userId
+      }
+    });
+
+    //then delete the parent comment
+    return await prisma.comment.delete({
+      where: {
+        id: id,
+        userId: userId
+      }
+    });
+  }
+}
+
+export const commentMethods = new Comment();
