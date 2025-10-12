54b76cc1099406021879ea8bdb831ad1c8652976
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-12 13:21:03 +0800

    Create a comment validator

---
+++ src/validator/commentValidator.ts
@@ -0,0 +1,45 @@
+import { body, param } from "express-validator";
+
+const isEmptyMsg = "must not be empty";
+
+export const validateCreateComment = [
+  body("comment").trim()
+    .notEmpty().withMessage(`Content ${isEmptyMsg}`),
+  param("postId")
+    .exists().withMessage("Post id doesn't exist")
+    .isUUID().withMessage("Post id must be a valid UUID") 
+]
+
+export const validateCreateChildComment = [
+  body("comment").trim()
+    .notEmpty().withMessage(`Content ${isEmptyMsg}`),
+  param("id")
+    .exists().withMessage("Comment id doesn't exist")
+    .isUUID().withMessage("Comment id must be a valid UUID")
+]
+
+export const validateGetAllComments = [
+  param("postId")
+    .exists().withMessage("Post id doesn't exist")
+    .isUUID().withMessage("Post id must be a valid UUID")
+]
+
+export const validateGetChildComments = [
+  param("id")
+    .exists().withMessage("Comment id doesn't exist")
+    .isUUID().withMessage("Comment id is not a valid UUID")
+]
+
+export const validateUpdateComment = [
+  body("comment").trim()
+    .notEmpty().withMessage(`Content ${isEmptyMsg}`),
+  param("id")
+  .exists().withMessage("Comment id doesn't exist")
+  .isUUID().withMessage("Comment id must be a valid UUID")
+]
+
+export const validateDeleteComment = [
+  param("id")
+  .exists().withMessage("Comment id doesn't exist")
+  .isUUID().withMessage("Comment id must be a valid UUID")
+]
