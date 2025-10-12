fe6017bb750db96396c93de3994d46d9c2ec0713
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-12 14:15:26 +0800

    Added a validateDeleteLikedPost

--- src/validator/likedPostValidator.ts
+++ src/validator/likedPostValidator.ts
@@ -7,6 +7,12 @@ export const validateSaveLikedPost = [
 ]
 
 export const validateUndoLikedPost = [
+  param("postId")
+  .exists().withMessage("Post id doesn't exist")
+  .isUUID().withMessage("Post id is not a valid UUID"),
+]
+
+export const validateDeleteLikedPost = [
   param("id")
   .exists().withMessage("Liked id doesn't exist")
   .isUUID().withMessage("Liked id is not a valid UUID"),
