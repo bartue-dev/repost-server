f4d8559fcd5901d519bb0278ff1f9f620c49911e
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-13 22:32:24 +0800

    Creates a allowedOrigins that takes a mutiple origins

---
+++ src/config/allowedOrigins.ts
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+  "http://localhost:5173", //react front end
+  "http://localhost:3000",
+  "http://localhost:3500",
+  "https://sitename.com" // production site name
+]
+
+export default allowedOrigins;
