42a023b3aa4947586e37fe85ea0c3f95ae04cb49
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-14 17:53:07 +0800

    import deleteReactions controller to have a /:id http endpoint

--- src/routes/reactionsRoutes.ts
+++ src/routes/reactionsRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createReactions,
+  deleteReactions,
   getReactionsFromPost
 } from "../controller/reactionsController.js"
 const router = Router();
@@ -8,5 +9,7 @@ const router = Router();
 router.route("/post/:postId")
   .post(createReactions)
   .get(getReactionsFromPost)
+
+router.delete("/:id", deleteReactions)
 
 export default router;
