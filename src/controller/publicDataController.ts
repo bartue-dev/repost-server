66d4cbe46fa12cedc031bf73870e94cd6160af1b
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-21 23:17:24 +0800

    Added a req.query to getPublicPost. If the query is an empty string or the varailble saving it is empty array the use the getPublicPost without parameter. But if query exist use the getPobstByTags query

--- src/controller/publicDataController.ts
+++ src/controller/publicDataController.ts
@@ -1,11 +1,44 @@
 import asyncHandler from "express-async-handler"
 import { publicMethods } from "../models/getPublicQueries.js";
 import { CustomErr } from "../middleware/customErr.js";
+import { validateSearchPostsByTags } from "../validator/publicDataValidator.js"; 
+import type { RequestHandler } from "express";
+import { validationResult } from "express-validator";
 
+//get public posts
+export const getPublicPost: RequestHandler[] = [
+  ...validateSearchPostsByTags
+  ,asyncHandler(async (req, res, next) => {
+  const { tags } = req.query;
 
-export const getPublicPost = asyncHandler(async (req, res, next) => {
+  //validation
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({
+      status: 400,
+      message: "validation error",
+      errors: errors.array()
+    });
+    return
+  }
 
-  const posts = await publicMethods.getPublicPost();
+  let tagsArr: string[] = []
+
+  //check first if tags is string then convert it to an array otherwise convert it into string and array
+  if (typeof tags === "string") {
+    tagsArr = tags.split(",").map(tag => tag.trim());
+  } else if (Array.isArray(tags)) {
+    tagsArr = tags.map(String).map(tag => tag.trim());
+  }
+
+  let posts;
+
+  //check if query is empty or then retrieve all posts otherwise retrieve posts base by tags query
+  if (tagsArr.length <= 0 || tags === "") {
+    posts = await publicMethods.getPublicPost();
+  } else if (tagsArr.length > 0) {
+    posts = await publicMethods.searchPostByTags(tagsArr);
+  }
 
   if (!posts) {
     const err = new CustomErr("Error on retrieving post", 400)
@@ -17,4 +50,4 @@ export const getPublicPost = asyncHandler(async (req, res, next) => {
     success: true,
     data: { posts }
   })
-})
+})];
