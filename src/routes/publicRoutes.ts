d2257ee9c6a2e5c4316e6339bb0024e77b8eb26d
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-21 23:19:07 +0800

    Added comment

--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import {getPublicPost} from "../controller/publicDataController.js"
+import {
+  getPublicPost,
+} from "../controller/publicDataController.js"
 const router = Router();
 
+
+//get public posts
 router.get("/", getPublicPost);
 
 export default router;
