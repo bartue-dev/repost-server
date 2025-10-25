3b85e19699889bc7d2dc40e30ef0161755ca7c9b
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-25 20:19:02 +0800

    Creates a get route with /:postId endpoint using getSpecificPublicPost controller

--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import {
   getPublicPost,
+  getSpecificPublicPost,
 } from "../controller/publicDataController.js"
 const router = Router();
 
 
 //get public posts
 router.get("/", getPublicPost);
+router.get("/:postId", getSpecificPublicPost)
 
 export default router;
