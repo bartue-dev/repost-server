0fed4c4517f33f1627cdb0656f497f09633a9a83
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-25 20:19:25 +0800

    Creates a validateGetSpecificPublicPost

--- src/validator/publicDataValidator.ts
+++ src/validator/publicDataValidator.ts
@@ -1,8 +1,12 @@
-import { query} from "express-validator";
-
-const isEmptyMsg = "must not be empty"
+import { query, param } from "express-validator";
 
 export const validateSearchPostsByTags = [
   query("tags").optional()
     .isString().withMessage("Search tags must be a string")
+]
+
+export const validateGetSpecificPublicPosts = [
+  param("postId")
+    .exists().withMessage("Post id doesn't exist")
+    .isUUID().withMessage("Post id is not a valid UUID")
 ]
