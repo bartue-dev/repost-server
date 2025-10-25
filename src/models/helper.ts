b3db2fbb1c1a9b394ce71f3d3ebb0c3dcf9a7d33
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-25 20:18:30 +0800

    Added a user to the included nested recursion. So it would return the user data all over the return comments and childComments

--- src/models/helper.ts
+++ src/models/helper.ts
@@ -3,15 +3,20 @@ import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
 
 type Query = {
-  childComment: boolean | {include: Query}
+  childComment: boolean | {include: Query},
+  user: boolean
+
 }
 
 //helper, to hanldle how many child comment include in getAllPost query
 export function includeComment(levels: number) {
-  let query:Query = { childComment: true };
+  let query:Query = { childComment: true, user: true };
 
   for (let i = 1; i < levels; i++) {
-    query = { childComment: {include: query} }
+    query = { 
+      user: true,
+      childComment: {include: query}
+    }
   }
 
   return query
